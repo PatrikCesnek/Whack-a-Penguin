82361ccfd0c3fbff99e2dbf9626944fc7ec5652b
PatrikCesnek <pcesnek290@gmail.com>
2020-11-22 15:05:42 +0100

    Add holes and penguines

---
+++ Whack-a-Penguin/WhackSlot.swift
@@ -0,0 +1,32 @@
+//
+//  WhackHole.swift
+//  Whack-a-Penguin
+//
+//  Created by Patrik Cesnek on 22/11/2020.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+
+        addChild(cropNode)
+    }
+}
