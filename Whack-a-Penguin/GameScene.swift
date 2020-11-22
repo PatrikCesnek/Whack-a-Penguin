631b803ab7edad51b06a372fc8269847f3963428
PatrikCesnek <pcesnek290@gmail.com>
2020-11-22 14:26:07 +0100

    Initial Commit

---
+++ Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by Patrik Cesnek on 22/11/2020.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
